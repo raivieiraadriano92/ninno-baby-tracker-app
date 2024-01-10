355f4ff18274c740c163b5a8d9d977de49420bfa
Raí Adriano <raivieiraadriano92@gmail.com>
2024-01-10 15:35:06 -0300

    feat: implemented welcome screen

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,10 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
-    plugins: ['nativewind/babel'],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin' // the Reanimated plugin has to be the last item in the plugins array
+    ],
     presets: ['babel-preset-expo']
   }
 }
--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -1,9 +1,26 @@
-import { Button, View } from 'react-native'
+import { Image, View } from 'react-native'
+import Animated, { FadeInUp } from 'react-native-reanimated'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Button, Text } from 'src/components'
 
 import type { RootStackScreen } from 'src/navigation/types'
 
+const AnimatedSafeAreaView = Animated.createAnimatedComponent(SafeAreaView)
+
 export const WelcomeScreen: RootStackScreen<'Welcome'> = ({ navigation }) => (
-  <View>
-    <Button onPress={() => navigation.navigate('Tabs')} title="Tabs" />
-  </View>
+  <AnimatedSafeAreaView className="flex-1 justify-between p-10" entering={FadeInUp}>
+    <View className="space-y-5">
+      <Text bold className="text-3xl text-center">
+        Welcome to
+      </Text>
+      <Image className="self-center" source={require('assets/logo-ninno.png')} />
+    </View>
+    <View className="space-y-10">
+      <Image className="self-center" source={require('assets/logo-ninno-face.png')} />
+      <Text className="text-center" medium>
+        Keep track of baby's feeding, sleeps, diapers & growth in one place.
+      </Text>
+    </View>
+    <Button onPress={() => navigation.navigate('Tabs')} title="Get started" />
+  </AnimatedSafeAreaView>
 )
