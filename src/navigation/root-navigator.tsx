e4830f77c8fa0eea173c673c2a312dda2a11c5e3
Raí Adriano <raivieiraadriano92@gmail.com>
2024-01-10 15:09:12 -0300

    feat: applied theme colors

--- src/navigation/root-navigator.tsx
+++ src/navigation/root-navigator.tsx
@@ -2,6 +2,7 @@ import type { FunctionComponent } from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { WelcomeScreen } from 'src/screens'
+import colors from 'src/theme/colors'
 
 import { TabNavigator } from './tab-navigator'
 
@@ -12,6 +13,10 @@ const NativeStack = createNativeStackNavigator<RootStackParamList>()
 export const RootNavigator: FunctionComponent = () => (
   <NativeStack.Navigator initialRouteName="Welcome">
     <NativeStack.Screen component={TabNavigator} name="Tabs" options={{ headerShown: false }} />
-    <NativeStack.Screen component={WelcomeScreen} name="Welcome" />
+    <NativeStack.Screen
+      component={WelcomeScreen}
+      name="Welcome"
+      options={{ contentStyle: { backgroundColor: colors.custom.blue4 }, headerShown: false }}
+    />
   </NativeStack.Navigator>
 )
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,18 +1,15 @@
 import { DefaultTheme } from '@react-navigation/native'
 
+import type { Theme } from '@react-navigation/native'
+
 const colors = require('src/theme/colors')
 
-export const theme = {
+export const theme: Theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    ...colors,
-    // card: '#FFFFFF',
-    text: colors.primary
-    //   border: 'rgb(216, 216, 216)',
-    //   notification: 'rgb(255, 59, 48)',
-    // ---DefaultTheme---
+    background: colors.custom.background,
+    primary: colors.custom.primary,
+    text: colors.custom.primary
   }
 }
-
-console.log(JSON.stringify(theme))
