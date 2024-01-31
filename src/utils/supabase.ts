1ba37ce4b34a8cf5148229ee593f50e160e53545
Raí Adriano <raivieiraadriano92@gmail.com>
2024-01-31 15:09:11 -0300

    feat: added supabase-js

---
+++ src/utils/supabase.ts
@@ -0,0 +1,25 @@
+import 'react-native-url-polyfill/auto'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+
+if (!supabaseUrl) {
+  throw new Error('Missing env.EXPO_PUBLIC_SUPABASE_URL')
+}
+
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
+
+if (!supabaseAnonKey) {
+  throw new Error('Missing env.EXPO_PUBLIC_SUPABASE_KEY')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false
+  }
+})
