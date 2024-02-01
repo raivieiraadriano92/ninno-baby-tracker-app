068bfc0742b8f70580bab80e180a4b290941a544
Raí Adriano <raivieiraadriano92@gmail.com>
2024-02-01 19:51:40 -0300

    feat: auto refresh supabase token

--- App.tsx
+++ App.tsx
@@ -4,13 +4,27 @@ import { useCallback } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
+import { AppState } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { usePreloadApp } from 'src/hooks'
 import { RootNavigator } from 'src/navigation/root-navigator'
 import { theme } from 'src/theme'
+import { supabase } from 'src/utils/supabase'
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync()
+
+// Tells Supabase Auth to continuously refresh the session automatically if
+// the app is in the foreground. When this is added, you will continue to receive
+// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
+// if the user's session is terminated. This should only be registered once.
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
 
 const App: FunctionComponent = () => {
   const { appIsReady } = usePreloadApp()
