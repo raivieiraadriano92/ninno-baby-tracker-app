3e401d8551f36c659705fe061c05e8ac1ad8e70e
Raí Adriano <raivieiraadriano92@gmail.com>
2024-02-08 11:05:48 -0300

    feat: redirect user to home screen once he is logged in

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
 import type { FunctionComponent } from 'react'
-import { useCallback } from 'react'
 
-import { NavigationContainer } from '@react-navigation/native'
-import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { AppState } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { usePreloadApp } from 'src/hooks'
 import { RootNavigator } from 'src/navigation/root-navigator'
-import { theme } from 'src/theme'
 import { supabase } from 'src/utils/supabase'
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync()
 
 // Tells Supabase Auth to continuously refresh the session automatically if
 // the app is in the foreground. When this is added, you will continue to receive
@@ -26,32 +18,11 @@ AppState.addEventListener('change', (state) => {
   }
 })
 
-const App: FunctionComponent = () => {
-  const { appIsReady } = usePreloadApp()
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync()
-    }
-  }, [appIsReady])
-
-  if (!appIsReady) {
-    return null
-  }
-
-  return (
-    <GestureHandlerRootView className="flex-1">
-      <NavigationContainer onReady={onLayoutRootView} theme={theme}>
-        <RootNavigator />
-        <StatusBar style="dark" />
-      </NavigationContainer>
-    </GestureHandlerRootView>
-  )
-}
+const App: FunctionComponent = () => (
+  <GestureHandlerRootView className="flex-1">
+    <RootNavigator />
+    <StatusBar style="dark" />
+  </GestureHandlerRootView>
+)
 
 export default App
--- src/navigation/root-navigator.tsx
+++ src/navigation/root-navigator.tsx
@@ -1,44 +1,97 @@
-import type { FunctionComponent } from 'react'
+import { useState, type FunctionComponent, useEffect } from 'react'
+import { useCallback } from 'react'
 
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import * as SplashScreen from 'expo-splash-screen'
+import { usePreloadApp } from 'src/hooks'
 import {
   BabyProfileCreationScreen,
   RecordFormScreen,
   RecordsScreen,
   WelcomeScreen
 } from 'src/screens'
+import { theme } from 'src/theme'
 import colors from 'src/theme/colors'
+import { supabase } from 'src/utils/supabase'
 
 import { TabNavigator } from './tab-navigator'
 
 import type { RootStackParamList } from './types'
+import type { Session } from '@supabase/supabase-js'
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync()
 
 const NativeStack = createNativeStackNavigator<RootStackParamList>()
 
-export const RootNavigator: FunctionComponent = () => (
-  <NativeStack.Navigator
-    initialRouteName="Tabs"
-    screenOptions={{
-      contentStyle: { backgroundColor: colors.custom.background },
-      headerBackTitleVisible: false,
-      headerTintColor: colors.custom.primary,
-      headerTitleStyle: { fontFamily: 'Nunito_700Bold' }
-    }}>
-    <NativeStack.Screen component={TabNavigator} name="Tabs" options={{ headerShown: false }} />
-    <NativeStack.Screen
-      component={BabyProfileCreationScreen}
-      name="BabyProfileCreation"
-      options={{
-        headerTitle: '',
-        headerTransparent: true
-      }}
-    />
-    <NativeStack.Screen component={RecordFormScreen} name="RecordForm" />
-    <NativeStack.Screen component={RecordsScreen} name="Records" />
-    <NativeStack.Screen
-      component={WelcomeScreen}
-      name="Welcome"
-      options={{ contentStyle: { backgroundColor: colors.custom.blue4 }, headerShown: false }}
-    />
-  </NativeStack.Navigator>
-)
+export const RootNavigator: FunctionComponent = () => {
+  const { appIsReady } = usePreloadApp()
+
+  const [session, setSession] = useState<Session | null | undefined>(undefined)
+
+  console.log('session', session)
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && session !== undefined) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady, session])
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  if (!appIsReady || session === undefined) {
+    return null
+  }
+
+  return (
+    <NavigationContainer onReady={onLayoutRootView} theme={theme}>
+      <NativeStack.Navigator
+        screenOptions={{
+          contentStyle: { backgroundColor: colors.custom.background },
+          headerBackTitleVisible: false,
+          headerTintColor: colors.custom.primary,
+          headerTitleStyle: { fontFamily: 'Nunito_700Bold' }
+        }}>
+        {session ? (
+          <>
+            <NativeStack.Screen
+              component={TabNavigator}
+              name="Tabs"
+              options={{ headerShown: false }}
+            />
+            <NativeStack.Screen
+              component={BabyProfileCreationScreen}
+              name="BabyProfileCreation"
+              options={{
+                headerTitle: '',
+                headerTransparent: true
+              }}
+            />
+            <NativeStack.Screen component={RecordFormScreen} name="RecordForm" />
+            <NativeStack.Screen component={RecordsScreen} name="Records" />
+          </>
+        ) : (
+          <NativeStack.Screen
+            component={WelcomeScreen}
+            name="Welcome"
+            options={{ contentStyle: { backgroundColor: colors.custom.blue4 }, headerShown: false }}
+          />
+        )}
+      </NativeStack.Navigator>
+    </NavigationContainer>
+  )
+}
