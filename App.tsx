968f6759341231cd95ee7ef216f0527b6581a01c
Raí Adriano <raivieiraadriano92@gmail.com>
2024-07-05 11:21:10 -0300

    set up nativewind

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
 import { FunctionComponent } from "react";
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const App: FunctionComponent = () => (
-  <View style={styles.container}>
+  <View className="bg-white flex-1 items-center justify-center">
     <Text>Open up App.tsx to start working on your app!</Text>
     <StatusBar style="auto" />
   </View>
 );
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
 
   return {
+    plugins: ["nativewind/babel"],
     presets: ["babel-preset-expo"]
   };
 };
