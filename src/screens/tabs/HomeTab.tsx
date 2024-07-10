47253ffe120b9cd19f7496aff66447da49ab2b60
Raí Adriano <raivieiraadriano92@gmail.com>
2024-07-10 20:44:45 -0300

    add baby info in home

--- src/screens/tabs/HomeTab.tsx
+++ src/screens/tabs/HomeTab.tsx
@@ -1,8 +1,6 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { Image } from "expo-image";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import colors from "tailwindcss/colors";
 
 import { Text } from "src/components/Text";
 import { UpgradeButton } from "src/components/UpgradeButton";
@@ -16,16 +14,26 @@ export const HomeTab: TabScreen<"Home"> = ({ navigation }) => (
     >
       <View className="flex-row items-center space-x-2">
         <Image
-          className="border-2 border-sky-400 h-12 rounded-xl w-12"
+          className="border-2 border-sky-400 h-14 rounded-xl w-14"
           source="https://picsum.photos/seed/696/3000/2000"
           contentFit="cover"
         />
         <View>
           <Text className="font-bold text-lg">Jimmy</Text>
-          <Text className="text-neutral-800 text-sm">5 months 15 days</Text>
+          <View className="flex-row space-x-1">
+            <View className="bg-sky-400 px-2 py-1 rounded-full">
+              <Text className="text-sky-100 text-xs">5 months 15 days</Text>
+            </View>
+            <View className="bg-sky-400 px-2 py-1 rounded-full">
+              <Text className="text-sky-100 text-xs">5kg</Text>
+            </View>
+            <View className="bg-sky-400 px-2 py-1 rounded-full">
+              <Text className="text-sky-100 text-xs">50cm</Text>
+            </View>
+          </View>
         </View>
       </View>
-      <Ionicons name="menu" size={24} color={colors.black} />
+      {/* <Ionicons name="people" size={24} color={colors.black} /> */}
     </SafeAreaView>
     <View className="flex-1 p-6">
       <UpgradeButton onPress={() => navigation.navigate("Upgrade")} />
