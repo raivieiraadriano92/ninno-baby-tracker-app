c77a2f0b677ecfb0a09506ba5f7b8939fc1c3129
Raí Adriano <raivieiraadriano92@gmail.com>
2024-07-23 12:05:30 -0300

    add mock data and activities type

--- src/screens/tabs/HomeTab.tsx
+++ src/screens/tabs/HomeTab.tsx
@@ -1,13 +1,38 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { format, isToday } from "date-fns";
 import { Image } from "expo-image";
-import { ScrollView, View } from "react-native";
+import { Pressable, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import colors from "tailwindcss/colors";
 
 import { Text } from "src/components/Text";
+import activities from "src/data/activities.json";
 import { TabScreen } from "src/navigation/types";
 
+const activityType = {
+  nursing: { color: colors.rose, emoji: "🤱" },
+  expressed: { color: colors.rose, emoji: "🍼" },
+  formula: { color: colors.rose, emoji: "🍼" },
+  supplement: { color: colors.rose, emoji: "🥣" },
+  diaper: { color: colors.amber, emoji: "💩" },
+  sleep: { color: colors.sky, emoji: "😴" },
+  growth: { color: colors.lime, emoji: "🌱" },
+  milestone: { color: colors.lime, emoji: "🎯" },
+  other: { color: colors.lime, emoji: "🧸" },
+  joy: { color: colors.lime, emoji: "😃" },
+  temperature: { color: colors.lime, emoji: "🌡️" },
+  medication: { color: colors.lime, emoji: "💊" },
+  vaccine: { color: colors.lime, emoji: "💉" }
+};
+
+const todaysActivities = activities.filter((item) => isToday(item.date)) as {
+  notes: string;
+  type: keyof typeof activityType;
+  date: string;
+}[];
+
 export const HomeTab: TabScreen<"Home"> = ({}) => (
-  <ScrollView>
+  <ScrollView showsVerticalScrollIndicator={false}>
     <SafeAreaView className="p-6" edges={["top"]}>
       <View className="bg-sky-300 h-40 items-center rounded-2xl">
         <Image
@@ -20,31 +45,38 @@ export const HomeTab: TabScreen<"Home"> = ({}) => (
       <Text className="font-medium text-sky-500 text-sm text-center">
         5 months 15 days
       </Text>
-      <Text className="font-bold my-6 text-lg">Today's Activities</Text>
+      <View className="flex-row items-center justify-between">
+        <Text className="font-bold my-6 text-lg">Today's Activities</Text>
+        <View className="flex-row items-center space-x-4">
+          <Pressable>
+            <Ionicons name="list" size={24} color={colors.sky[500]} />
+          </Pressable>
+          <Pressable>
+            <Ionicons name="stats-chart" size={24} color={colors.sky[500]} />
+          </Pressable>
+        </View>
+      </View>
       <View className="space-y-3">
-        {[
-          ["Feeding", colors.orange, "🍼"],
-          ["Sleep", colors.blue, "😴"],
-          ["Diaper", colors.green, "💩"],
-          ["Growth", colors.yellow, "🪴"]
-        ].map((item) => (
+        {todaysActivities.map((item, index) => (
           <View
             className="bg-white flex-row items-center p-3 rounded-2xl space-x-3"
-            key={item[0]}
-            style={{ backgroundColor: item[1][50] }}
+            key={index}
+            style={{ backgroundColor: activityType[item.type].color[50] }}
           >
             <View
               className="h-14 items-center justify-center rounded-lg w-14"
-              style={{ backgroundColor: item[1][200] }}
+              style={{ backgroundColor: activityType[item.type].color[200] }}
             >
-              <Text className="text-xl">{item[2]}</Text>
+              <Text className="text-xl">{activityType[item.type].emoji}</Text>
             </View>
             <View className="flex-1">
-              <Text className="font-medium">{item[0]}</Text>
-              <Text className="font-light text-neutral-500 text-xs">5.8kg</Text>
+              <Text className="capitalize font-medium">{item.type}</Text>
+              <Text className="font-light text-neutral-500 text-xs">
+                {item.notes}
+              </Text>
             </View>
             <Text className="font-light text-neutral-500 text-xs">
-              10:00 AM
+              {format(new Date(item.date), "h:mm a")}
             </Text>
           </View>
         ))}
