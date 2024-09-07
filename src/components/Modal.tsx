e73f5fa8bd7bc21719b4e5d211c739c68f2a8ea8
Raí Adriano <raivieiraadriano92@gmail.com>
2024-09-07 12:03:55 -0300

    implemented custom animated modal and applied in NursingStopwatchModal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,75 @@
+import {
+  forwardRef,
+  PropsWithChildren,
+  useImperativeHandle,
+  useState
+} from "react";
+
+import { BlurView } from "expo-blur";
+import { Modal as RNModal, TouchableWithoutFeedback } from "react-native";
+import Animated, { SlideInDown, SlideOutDown } from "react-native-reanimated";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+type ModalProps = PropsWithChildren<{
+  onClose?: () => void;
+  onOpen?: () => void;
+}>;
+
+export type ModalRef = {
+  close: () => void;
+  open: () => void;
+};
+
+export const Modal = forwardRef<ModalRef, ModalProps>(
+  ({ children, onClose, onOpen }, ref) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const close = () => {
+      setIsVisible(false);
+
+      onClose?.();
+    };
+
+    const open = () => {
+      setIsVisible(true);
+
+      onOpen?.();
+    };
+
+    useImperativeHandle(ref, () => ({
+      close,
+      open
+    }));
+
+    return (
+      <RNModal animationType="fade" transparent visible={isVisible}>
+        <SafeAreaProvider>
+          <TouchableWithoutFeedback onPress={close}>
+            <BlurView
+              className="flex-1"
+              experimentalBlurMethod="dimezisBlurView"
+              intensity={100}
+              tint="dark"
+            >
+              <SafeAreaView className="flex-1 justify-end p-6">
+                {isVisible && (
+                  <TouchableWithoutFeedback onPress={() => {}}>
+                    <Animated.View
+                      className="bg-white p-6 rounded-2xl space-y-6 w-full"
+                      entering={SlideInDown}
+                      exiting={SlideOutDown}
+                    >
+                      {children}
+                    </Animated.View>
+                  </TouchableWithoutFeedback>
+                )}
+              </SafeAreaView>
+            </BlurView>
+          </TouchableWithoutFeedback>
+        </SafeAreaProvider>
+      </RNModal>
+    );
+  }
+);
+
+Modal.displayName = "Modal";
--- src/components/NursingStopwatchModal.tsx
+++ src/components/NursingStopwatchModal.tsx
@@ -1,28 +1,14 @@
-import {
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-  useState
-} from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { format } from "date-fns";
-import { BlurView } from "expo-blur";
 import { activateKeepAwakeAsync, deactivateKeepAwake } from "expo-keep-awake";
-import {
-  Modal,
-  Switch,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View
-} from "react-native";
-import Animated, { SlideInDown, SlideOutDown } from "react-native-reanimated";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Switch, TouchableOpacity, View } from "react-native";
 import colors from "tailwindcss/colors";
 import { DefaultColors } from "tailwindcss/types/generated/colors";
 
 import { Button } from "./Button";
+import { Modal, ModalRef } from "./Modal";
 import { Text } from "./Text";
 
 import { NursingSide } from "src/services/database/models/ActivityModel";
@@ -36,43 +22,53 @@ export type NursingStopwatchModalRef = {
   start: (_: { initialValueInMinutes: number; side: NursingSide }) => void;
 };
 
+const INITIAL_STATE = {
+  seconds: 0,
+  isPaused: false,
+  keepAwake: true,
+  side: NursingSide.LEFT
+};
+
 export const NursingStopwatchModal = forwardRef<
   NursingStopwatchModalRef,
   NursingStopwatchModalProps
 >(({ color, onSave }, ref) => {
   const [state, setState] = useState({
-    isVisible: false,
-    seconds: 0,
-    isPaused: false,
-    keepAwake: true,
-    side: NursingSide.LEFT
+    ...INITIAL_STATE
   });
 
   const refInterval = useRef<NodeJS.Timeout>();
 
+  const refModal = useRef<ModalRef>(null);
+
+  const resetInterval = () => {
+    if (refInterval.current) {
+      clearInterval(refInterval.current);
+    }
+
+    refInterval.current = undefined;
+  };
+
   const startTimer = () => {
+    resetInterval();
+
+    setState((prev) => ({
+      ...prev,
+      isPaused: false
+    }));
+
     refInterval.current = setInterval(() => {
       setState((prev) => ({
         ...prev,
-        seconds: prev.seconds + 1,
-        isPaused: prev.isPaused ? false : prev.isPaused
+        seconds: prev.seconds + 1
       }));
     }, 1000);
-  };
-
-  const resetInterval = () => {
-    clearInterval(refInterval.current);
-
-    refInterval.current = undefined;
   };
 
   const save = () => {
     resetInterval();
 
-    setState((prev) => ({
-      ...prev,
-      isVisible: false
-    }));
+    refModal.current?.close();
 
     onSave({
       durationInMinutes: Math.ceil(state.seconds / 60),
@@ -92,119 +88,100 @@ export const NursingStopwatchModal = forwardRef<
   const cancel = () => {
     resetInterval();
 
-    setState((prev) => ({
-      ...prev,
-      isVisible: false
-    }));
+    refModal.current?.close();
+  };
+
+  const handleKeepAwake = (keepAwake: boolean, changeState = true) => {
+    if (keepAwake) {
+      activateKeepAwakeAsync();
+
+      console.log("Keep awake activated");
+    } else {
+      console.log("Keep awake deactivated");
+
+      deactivateKeepAwake();
+    }
+
+    if (changeState) {
+      console.log("Keep awake state changed");
+
+      setState((prev) => ({ ...prev, keepAwake }));
+    }
   };
 
   useImperativeHandle(ref, () => ({
     start: ({ initialValueInMinutes, side }) => {
-      setState((prev) => ({
-        ...prev,
+      setState({
+        ...INITIAL_STATE,
         seconds: initialValueInMinutes * 60,
-        isVisible: true,
         side
-      }));
+      });
+
+      refModal.current?.open();
 
       startTimer();
     }
   }));
 
-  useEffect(() => {
-    if (state.keepAwake && state.isVisible) {
-      activateKeepAwakeAsync();
-    } else {
-      deactivateKeepAwake();
-    }
-  }, [state.keepAwake, state.isVisible]);
-
   return (
-    <Modal animationType="fade" transparent visible={state.isVisible}>
-      <SafeAreaProvider>
-        <TouchableWithoutFeedback onPress={cancel}>
-          <BlurView
-            className="flex-1"
-            experimentalBlurMethod="dimezisBlurView"
-            intensity={100}
-            tint="dark"
+    <Modal
+      onClose={() => handleKeepAwake(false, false)}
+      onOpen={() => handleKeepAwake(INITIAL_STATE.keepAwake, false)}
+      ref={refModal}
+    >
+      <Text
+        className="font-bold text-3xl text-center"
+        style={{ color: colors[color][500] }}
+      >
+        {format(state.seconds * 1000, "mm:ss")}
+      </Text>
+      <TouchableOpacity
+        className="self-center"
+        onPress={state.isPaused ? startTimer : pause}
+      >
+        <Ionicons
+          color={colors.neutral[300]}
+          name={state.isPaused ? "play" : "pause"}
+          size={48}
+        />
+      </TouchableOpacity>
+      <View className="border-neutral-100 border-[1px] px-4 rounded-2xl">
+        <View className="flex-row items-center h-13 justify-between space-x-2">
+          <Text className="text-sm" style={{ color: colors[color][500] }}>
+            Side
+          </Text>
+          <Text
+            className="capitalize font-bold"
+            style={{ color: colors[color][500] }}
           >
-            <SafeAreaView className="flex-1 justify-end p-6">
-              {state.isVisible && (
-                <TouchableWithoutFeedback onPress={() => {}}>
-                  <Animated.View
-                    className="bg-white p-6 rounded-2xl space-y-6 w-full"
-                    entering={SlideInDown}
-                    exiting={SlideOutDown}
-                  >
-                    <Text
-                      className="font-bold text-3xl text-center"
-                      style={{ color: colors[color][500] }}
-                    >
-                      {format(state.seconds * 1000, "mm:ss")}
-                    </Text>
-                    <TouchableOpacity
-                      className="self-center"
-                      onPress={state.isPaused ? startTimer : pause}
-                    >
-                      <Ionicons
-                        color={colors.neutral[300]}
-                        name={state.isPaused ? "play" : "pause"}
-                        size={48}
-                      />
-                    </TouchableOpacity>
-                    <View className="border-neutral-100 border-[1px] px-4 rounded-2xl">
-                      <View className="flex-row items-center h-13 justify-between space-x-2">
-                        <Text
-                          className="text-sm"
-                          style={{ color: colors[color][500] }}
-                        >
-                          Side
-                        </Text>
-                        <Text
-                          className="capitalize font-bold"
-                          style={{ color: colors[color][500] }}
-                        >
-                          {state.side}
-                        </Text>
-                      </View>
-                      <View className="bg-neutral-100 h-[1px]" />
-                      <View className="flex-row items-center h-13 justify-between space-x-2">
-                        <Text
-                          className="text-sm"
-                          style={{ color: colors[color][500] }}
-                        >
-                          Keep screen awake
-                        </Text>
-                        <Switch
-                          trackColor={{
-                            false: colors.neutral[300],
-                            true: colors[color][500]
-                          }}
-                          thumbColor={colors.white}
-                          ios_backgroundColor={colors.neutral[300]}
-                          onValueChange={(keepAwake) =>
-                            setState((prev) => ({ ...prev, keepAwake }))
-                          }
-                          value={state.keepAwake}
-                        />
-                      </View>
-                    </View>
-                    <View className="space-y-4">
-                      <Button
-                        customColors={[colors[color][500], colors[color][600]]}
-                        onPress={save}
-                        title="Save"
-                      />
-                      <Button onPress={cancel} title="Cancel" variant="link" />
-                    </View>
-                  </Animated.View>
-                </TouchableWithoutFeedback>
-              )}
-            </SafeAreaView>
-          </BlurView>
-        </TouchableWithoutFeedback>
-      </SafeAreaProvider>
+            {state.side}
+          </Text>
+        </View>
+        <View className="bg-neutral-100 h-[1px]" />
+        <View className="flex-row items-center h-13 justify-between space-x-2">
+          <Text className="text-sm" style={{ color: colors[color][500] }}>
+            Keep screen awake
+          </Text>
+          <Switch
+            trackColor={{
+              false: colors.neutral[300],
+              true: colors[color][500]
+            }}
+            thumbColor={colors.white}
+            ios_backgroundColor={colors.neutral[300]}
+            onValueChange={handleKeepAwake}
+            value={state.keepAwake}
+          />
+        </View>
+      </View>
+      <View className="space-y-4">
+        <Button
+          customColors={[colors[color][500], colors[color][600]]}
+          onPress={save}
+          title="Save"
+        />
+        <Button onPress={cancel} title="Cancel" variant="link" />
+      </View>
     </Modal>
   );
 });
